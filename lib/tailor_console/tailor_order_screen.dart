cd6ecf0a8a352b1f230ecaf48325f6ee4418c0ca
Usama <epiicuree@gmail.com>
2024-05-15 17:39:41 +0500

    seperate screen for tailor order

--- lib/tailor_console/tailor_order_screen.dart
+++ lib/tailor_console/tailor_order_screen.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fyp/user_console/user_order_screen.dart';
 
 class TailorOrderScreen extends StatelessWidget {
   const TailorOrderScreen({super.key});
@@ -81,131 +82,6 @@ class TailorOrderScreen extends StatelessWidget {
           }
         },
       ),
-    );
-  }
-}
-
-class OrderDetailScreen extends StatefulWidget {
-  final Map<String, dynamic> order;
-
-  const OrderDetailScreen({Key? key, required this.order}) : super(key: key);
-
-  @override
-  _OrderDetailScreenState createState() => _OrderDetailScreenState();
-}
-
-class _OrderDetailScreenState extends State<OrderDetailScreen> {
-  late String _selectedStatus;
-
-  @override
-  void initState() {
-    super.initState();
-    _selectedStatus = widget.order['status'];
-  }
-
-  void _updateStatus() {
-    FirebaseFirestore.instance
-        .collection('orders')
-        .where('tailorId',
-            isEqualTo: widget.order['tailorId']) // Filter by tailorId
-        .get()
-        .then((querySnapshot) {
-      // Iterate over the documents with the same tailorId
-      querySnapshot.docs.forEach((doc) {
-        // Update the status for each document
-        doc.reference.update({'status': _selectedStatus}).then((value) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Status updated successfully')),
-          );
-        }).catchError((error) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('Failed to update status: $error')),
-          );
-        });
-      });
-    }).catchError((error) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to update status: $error')),
-      );
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Order Details'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView(
-              children: const [
-                TrackingStep(title: 'Confirmed', time: '04:30pm'),
-                TrackingStep(title: 'Processing', time: '04:38pm'),
-                TrackingStep(title: 'On the way', time: '04:42pm'),
-                TrackingStep(title: 'Delivered', time: '04:46pm'),
-              ],
-            ),
-          ),
-          Card(
-            margin: const EdgeInsets.all(8),
-            child: Column(
-              children: [
-                const ListTile(
-                  title: Text('Total Bill'),
-                  subtitle: Text('\$43.35'),
-                  trailing: Icon(Icons.credit_card),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      DropdownButton<String>(
-                        value: _selectedStatus,
-                        onChanged: (value) {
-                          setState(() {
-                            _selectedStatus = value!;
-                          });
-                        },
-                        items: <String>['Pending', 'In-progress', 'Completed']
-                            .map((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      ),
-                      ElevatedButton(
-                        onPressed: _updateStatus,
-                        child: const Text('Update Status'),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class TrackingStep extends StatelessWidget {
-  final String title;
-  final String time;
-
-  const TrackingStep({Key? key, required this.title, required this.time})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(title),
-      subtitle: Text(time),
-      leading: const Icon(Icons.check_circle_outline),
     );
   }
 }
